1c4e729c38401a31c1704c01c206fc750424dff8
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2023-11-27 16:23:18 -0600

    🎧 Add support to switch between HDMI profiles
    If a sink is linked to a card that has multiple HDMI stereo
    profiles, switching sinks will also switch between these
    profiles. Profiles that are reported as unavailable
    (which typically means the HDMI output is disconnected)
    are skipped.
    
    It only considers stereo profile (not 5.1 or 7.1) for now.

--- plugins/pulse.py
+++ plugins/pulse.py
@@ -1,4 +1,6 @@
 import pulsectl
+import re
+
 
 class Pulse(object):
 
@@ -23,9 +25,28 @@ class Pulse(object):
 
   def get_default_sink(self):
     sink = self.pulse.get_sink_by_name(self.pulse.server_info().default_sink_name)
-    return "🎧️\n" + sink.description.split()[0]
+    card = [ card for card in self.pulse.card_list() if card.index==sink.card ][0]
+    hdmi_ports = re.findall(".* Stereo \((.*)\) Output", card.profile_active.description)
+    if hdmi_ports:
+      extra_port_info = hdmi_ports[0]
+      if extra_port_info == "HDMI":
+        extra_port_info = "HDMI 1"
+      extra_port_info = "\n" + extra_port_info
+    else:
+      extra_port_info = ""
+    return "🎧️\n" + sink.description.split()[0] + extra_port_info
 
   def next_default_sink(self):
+    sink = self.pulse.get_sink_by_name(self.pulse.server_info().default_sink_name)
+    card = [ card for card in self.pulse.card_list() if card.index==sink.card ][0]
+    hdmi_profiles = [ p for p in card.profile_list if p.available and p.name.startswith("output:hdmi-stereo") ]
+    if hdmi_profiles:
+      active_profile_index = [ p.name for p in hdmi_profiles].index(card.profile_active.name)
+      active_profile_index += 1
+      if active_profile_index < len(hdmi_profiles):
+        self.pulse.card_profile_set(card, hdmi_profiles[active_profile_index])
+        return
+      self.pulse.card_profile_set(card, card.profile_list[0])
     current_sink_name = self.pulse.server_info().default_sink_name
     sink_list = self.pulse.sink_list()
     next_sink = sink_list[-1]
